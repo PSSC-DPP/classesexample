d1ff9dceb5a73517d878e00e637a6eb68277ca0c
PSSC-DPP <pssc.digipen@gmail.com>
2020-11-15 17:56:54 -0800

    wrgfasdf

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.vx = sprite.x - otherSprite.x
+    sprite.vy = sprite.y - otherSprite.y
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(4)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(-1)
+})
 let magV = 0
 let magVsq = 0
 let speed = 20
@@ -5,46 +15,61 @@ let player2 = sprites.create(sprites.food.smallPizza, SpriteKind.Player)
 // let enemy = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Enemy)
 controller.moveSprite(player2, 100, 100)
 let enemies = sprites.allOfKind(SpriteKind.Enemy)
-let numEnemies = 7
+let numEnemies = 4
 // let mysprite = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Enemy)
 for (let index = 0; index <= numEnemies - 1; index++) {
+    // initialize
+    // initialize
+    // initialize
+    // initialize
+    // initialize
+    // initialize
     // initialize
     // initialize
     enemies[index] = sprites.create(sprites.castle.heroWalkFront1, SpriteKind.Enemy)
     enemies[index].x = randint(10, screen.width - 10)
     enemies[index].y = randint(10, screen.height - 10)
 }
-class Orc 
+class Orc // the spicy one
 {
     name: string
-    age: number
+    spice: number
     orken: Sprite
-    constructor(nameOrc:string, ageOrc:number) 
+    constructor(nameOrc:string, spiceOrc:number) 
     {
         this.name = nameOrc;
-        this.age = ageOrc;
+        this.spice = spiceOrc;
         this.orken = sprites.create(sprites.castle.shrub, SpriteKind.Food)
-        this.orken.setPosition(scrMidX,scrMidY)
+        this.orken.setPosition(randint(10, screen.width - 10),randint(10, screen.height - 10))
         this.orken.setFlag(SpriteFlag.StayInScreen, true)
     }
 }
 let bob = new Orc("Bob",4)
-let scrMidX = scene.screenWidth()/2
-let scrMidY = scene.screenHeight()/2
-
-game.onUpdateInterval(500, function () {
-	bob.age += 1
-    bob.orken.say("" + bob.name + " " + Math.trunc(bob.age))
-    bob.orken.ax = ( randint(0,bob.age) - randint(0,bob.age) )
-    bob.orken.ay = ( randint(0,bob.age) - randint(0,bob.age) )
-    if (bob.orken.x != player2.x) {
-        bob.orken.vx = player2.x - bob.orken.x
-    }
-    if (bob.orken.y != player2.y) {
-        bob.orken.vy = player2.y - bob.orken.y
+let scrMidX = scene.screenWidth() / 2
+let scrMidY = scene.screenHeight() / 2
+// more = more ADHD
+let spiceFactor = 1000000
+// more = more focus
+let velFactor = 0.5
+game.onUpdate(function () {
+    if (info.score() > 1024) {
+        game.over(true, effects.smiles)
     }
 })
-game.onUpdateInterval(200, function () { 
+// more = more focus
+game.onUpdateInterval(100, function () {
+    bob.spice += 1
+bob.orken.say("" + bob.name + " " + Math.trunc(bob.spice))
+    bob.orken.ax = ( randint(0,bob.spice) - randint(0,bob.spice) ) * spiceFactor
+bob.orken.ay = ( randint(0,bob.spice) - randint(0,bob.spice) ) * spiceFactor
+if (bob.orken.x != player2.x) {
+        bob.orken.vx = ( player2.x - bob.orken.x ) * velFactor
+    }
+    if (bob.orken.y != player2.y) {
+        bob.orken.vy = ( player2.y - bob.orken.y ) * velFactor
+    }
+})
+game.onUpdateInterval(200, function () {
     for (let m = 0; m <= enemies.length - 1; m++) {
         if (enemies[m].x != player2.x) {
             enemies[m].vx = player2.x - enemies[m].x
